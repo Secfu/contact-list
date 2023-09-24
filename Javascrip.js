c36c41d3c7b9c240ff8277236e77f2048a58882a
secfu <secfu1603@gmail.com>
2023-09-24 13:31:50 -0600

    arreglar el error para el pull recuest

--- Javascrip.js
+++ Javascrip.js
@@ -1,3 +1,40 @@
+
+// 1. Crea una lista de contactos con datos predefinidos, cada contacto debe contener el nombre y apellido como una sola cadena de caracteres
+// let listContact = ['Jairo Menjevar', 'Carlos Darias', 'jonh smith', 'Poncho Matias'];
+
+// Crea una función para añadir un nuevo contacto a una lista
+// function addContact(contact){
+//     contactList.push(contact);
+// }
+
+// addContact("Saul Cruz");
+
+
+//Crea una función para borrar un contacto existente de la lista
+// function contactDelete(nombreCompleto) {
+//   const indice = contactList.indexOf(nombreCompleto);
+//   if (indice !== -1) {
+//     contactList.splice(indice, 1);
+//     console.log(`Se ha borrado el contacto "${nombreCompleto}" de la lista.`);
+//   } else {
+//     console.log(`No se encontró el contacto "${nombreCompleto}" en la lista.`);
+//   }
+// }
+
+
+// contactDelete("Carlos Darias");
+
+
+//Crea una función para imprimir en consola los contactos presentes en la lista
+// function contactPrint() {
+//   console.log("Lista de Contactos:");
+//   for (let i = 0; i < contactList.length; i++) {
+//     console.log(contactList[i]);
+//   }
+// }
+
+
+// contactPrint();
 //////////////////////////////////////////////////////////////////////////////////////////////////
 ////////// Modificacion de codigo para la lista de contactos  ////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////
